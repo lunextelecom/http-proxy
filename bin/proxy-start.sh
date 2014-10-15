a395e79d0f529ed54a90570195fa009d77c08ab1
wqjung <wqjung@gmail.com>
2014-10-15 00:02:17 -0400

    add script

---
+++ bin/proxy-start.sh
@@ -0,0 +1,40 @@
+!/bin/bash
+DEPLOY_DIR=$(dirname $0)/..
+PARGS=$@
+APP=$(echo "$PARGS" | sed -r 's/.*-a ([^ ]+).*/\1/g')
+CONF=$(echo "$PARGS" | sed -r 's/.*-c ([^ ]+).*/\1/g')
+LOG=$(echo "$PARGS" | sed -r 's/.*-l ([^ ]+).*/\1/g')
+
+
+if [ -z "$APP" ]; then
+        APP=$DEPLOY_DIR/conf/app.properties
+fi
+if [ -z "$CONF" ]; then
+        CONF=$DEPLOY_DIR/conf/configuration.yaml
+fi
+if [ -z "$LOG" ]; then
+        LOG=$DEPLOY_DIR/conf/log4j.properties
+fi
+
+f=$APP
+if [ -d "$f" ]; then f=$f/.; fi
+absolute=$(cd "$(dirname -- "$f")"; printf %s. "$PWD")
+absolute=${absolute%?}
+absolute=$absolute/${f##*/}
+APP=$absolute
+
+f=$CONF
+if [ -d "$f" ]; then f=$f/.; fi
+absolute=$(cd "$(dirname -- "$f")"; printf %s. "$PWD")
+absolute=${absolute%?}
+absolute=$absolute/${f##*/}
+CONF=$absolute
+
+f=$LOG
+if [ -d "$f" ]; then f=$f/.; fi
+absolute=$(cd "$(dirname -- "$f")"; printf %s. "$PWD")
+absolute=${absolute%?}
+absolute=$absolute/${f##*/}
+LOG=$absolute
+
+java -jar -Xms2500m -Xmx2500m $DEPLOY_DIR/lib/http-proxy-1.0-SNAPSHOT.jar -a $APP -c $CONF -l $LOG
