f60dd9767a449b5a1dd102c364828d87739ac217
duynguyen-lunex <duynguyen@lunextelecom.com>
2014-10-15 16:03:28 +0700

    update script deploy

--- bin/proxy-start.sh
+++ bin/proxy-start.sh
@@ -3,6 +3,7 @@ DEPLOY_DIR=$(dirname $0)/..
 PARGS=$@
 APP=$(echo "$PARGS" | sed -r 's/.*-a ([^ ]+).*/\1/g')
 CONF=$(echo "$PARGS" | sed -r 's/.*-c ([^ ]+).*/\1/g')
+LOG=$(echo "$PARGS" | sed -r 's/.*-l ([^ ]+).*/\1/g')
 
 
 if [ -z "$APP" ]; then
@@ -11,7 +12,9 @@ fi
 if [ -z "$CONF" ]; then
         CONF=$DEPLOY_DIR/conf/configuration.yaml
 fi
-
+if [ -z "$LOG" ]; then
+        LOG=$DEPLOY_DIR/conf/log4j.properties
+fi
 
 f=$APP
 if [ -d "$f" ]; then f=$f/.; fi
@@ -27,4 +30,11 @@ absolute=${absolute%?}
 absolute=$absolute/${f##*/}
 CONF=$absolute
 
-java -jar -Xms2500m -Xmx2500m $DEPLOY_DIR/lib/http-proxy-1.0-SNAPSHOT.jar -a $APP -c $CONF
+f=$LOG
+if [ -d "$f" ]; then f=$f/.; fi
+absolute=$(cd "$(dirname -- "$f")"; printf %s. "$PWD")
+absolute=${absolute%?}
+absolute=$absolute/${f##*/}
+LOG=$absolute
+
+java -jar -Xms2500m -Xmx2500m -Dlog4j.configuration=file:$LOG $DEPLOY_DIR/lib/http-proxy-1.0-SNAPSHOT.jar -a $APP -c $CONF
